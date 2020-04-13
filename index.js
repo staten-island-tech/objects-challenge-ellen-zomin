d135e4f8bafda2d9106975212f936f6977d8045b
ellen-zomin <57499060+ellen-zomin@users.noreply.github.com>
2020-04-13 12:49:49 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,44 @@
 ///Objects
-const Omar = {
+/*const Omar = {
     name: "Omar",
     dob: 2003, 
     graduated: false,
-    
-}
+    age: function(){
+        //console.log(this);
+        return 2020 - this.dob;
+    }
+};
+*/
+
+/*const Homer = {
+    name: "Homer",
+    age:32,
+    family:{
+        wife:{
+            name: "Marge",
+            age: 32
+        },
+        son:{
+            name: "Bart",
+            age: 10
+        },
+    }
+    pets: ["Cat", "Dog"] 
+}; 
+*/
+
+//////Destructuring
+
+/*const Sandra = {
+    name:"Sandra",
+    age:17
+    student: true,
+    year: "junior",
+    graduated: false
+};
+
+const {name, year} = Sandra;
+//const student = {};
+//student.name = Sandra.name;
+const student = Sandra; //this is not a copy*/
+
