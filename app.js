e5ba51d8c7a5eb95a6c102eebc63bebaf00956ec
ellen-zomin <57499060+ellen-zomin@users.noreply.github.com>
2020-04-13 12:59:02 -0400

    Update app.js

--- app.js
+++ app.js
@@ -23,3 +23,33 @@ const presidents = [
 //4) log all dems who served 2 terms. HINT use chain filter, filter and slice
 
 //BONUS write a script to check if LBJ was a 2 term president. IF he was then alert("LBJ served two terms") else alert "LBJ was one and done"
+
+
+
+
+//1) Filter all presidents, leaving only the Democratic ones
+
+const result = presidents.filter(president => president.party === "D")
+console.log(result)
+
+//2)Filter all presidents to leave only one term Republican presidents HINT use If statement
+console.log(presidents.filter(president => president.party === "R"
+&& president.terms === 1))
+
+//3) return only the last three presidents
+const presLeng = presidents.length
+console.log(presidents.slice(presLeng-3, presLeng))
+
+//4) log all dems who served 2 terms. HINT use chain filter, filter and slice
+console.log(presidents.filter(president => president.party === "D"
+&& president.terms === 2))
+
+//BONUS write a script to check if LBJ was a 2 term president. IF he was then alert("LBJ served two terms") else alert "LBJ was one and done"
+
+const LBJ = president[presidents.length-2]
+if(LBJ.terms === 2){
+  alert("LBJ served two terms")
+}
+else{
+  alert("LBJ was one and done")
+};
